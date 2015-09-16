6fc41fff85e57998687cfa08911954220ca3c2dc
bruceChina <827057760@qq.com>
2015-09-16 08:46:02 +0800

    新增匹配demo

--- RegexDemo/src/com/exxample/regex/RegexDemo3.java
+++ RegexDemo/src/com/exxample/regex/RegexDemo3.java
@@ -2,27 +2,27 @@ package com.exxample.regex;
 
 import java.util.Scanner;
 
-/**
- * 
- * @author Administrator
- *
- * 校验
- * 		1: 11位的数字
- * 		2: 第一位是1 第二位是3 或者 5
- * 		3: 接下来的是任意数字
+/*
+ * 校验:
+ * 		开头至少一个字符是单词字符
+ * 		单词字符之后跟着"@"
+ *      "@"跟着2-8单词字符
+ *      如果后面还有,则是"." +　2-3个单词字符
  */
-public class RegexDemo2 {
+public class RegexDemo3 {
+
 	public static void main(String[] args) {
-		String regex = "1[35][0-9]{9}";
+		String regex = "[a-zA-Z_0-9]+@[a-zA-Z_0-9]{2,8}(\\.[a-zA-Z_0-9]{2,3})+";
 		Scanner sc = new Scanner(System.in);
-		while(true){
+		while (true) {
 			System.out.println("请输入:");
 			String string = sc.nextLine();
-			if("over".equals(string)){
+			if ("over".equals(string)) {
 				break;
 			}
 			System.out.println(string.matches(regex));
 		}
 		sc.close();
 	}
+
 }
