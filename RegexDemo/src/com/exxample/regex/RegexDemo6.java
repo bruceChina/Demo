533d5d704b6b773b78b608f1e30dbf4f2e11cd10
bruceChina <827057760@qq.com>
2015-09-20 23:16:30 +0800

    叠词分割demo

---
+++ RegexDemo/src/com/exxample/regex/RegexDemo6.java
@@ -0,0 +1,17 @@
+package com.exxample.regex;
+/*
+ * 叠词切割
+ * 需求: "assdfffgghveggs"
+ * 		切割后剩下a d h v e s
+ */
+public class RegexDemo6 {
+	public static void main(String[] args) {
+		String string = "assdfffgghveggs";
+		String regex = "(.)\\1+";
+		String[] splits = string.split(regex);
+		System.out.println(splits.length);
+		for (String split : splits) {
+			System.out.println(split);
+		}
+	}
+}
